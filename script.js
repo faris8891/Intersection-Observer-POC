186ddf2bb7c67a64cdf119919cd4123f39d1d9d9
faris8891 <salmanulfaris.villoor@gmail.com>
2024-02-22 13:37:16 +0530

    feat: elements only observe one time

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const cards = document.querySelectorAll(".card");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    entry.target.classList.toggle("show", entry.isIntersecting);
-  });
-  console.log(entries);
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+      if (entry.isIntersecting) observer.unobserve(entry.target); // the element only observe one time
+    });
+  },
+  {
+    threshold: 1, //accept values-  0 to 1
+    rootMargin: "-100px", // we can control the margin offset, its better to use infinite observer before reach end
+  }
+);
 
 cards.forEach((card) => {
   observer.observe(card);
